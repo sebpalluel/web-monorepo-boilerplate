5c9d728d73ed0e85ede5e8dab20ce8d80f0bd87e
sebpalluel <sebpalluel@gmail.com>
2022-09-01 22:09:05 +0200

    changed references from sozonome repo to this one with corresponding reports in badges

--- apps/web/next-seo.config.js
+++ apps/web/next-seo.config.js
@@ -1,24 +1,24 @@
 /** @type {import('next-seo').DefaultSeoProps} */
 const defaultSEOConfig = {
-  title: 'nextarter-chakra',
-  titleTemplate: '%s | nextarter-chakra',
-  defaultTitle: 'nextarter-chakra',
+  title: 'web-monorepo-boilerplate',
+  titleTemplate: '%s | web-monorepo-boilerplate',
+  defaultTitle: 'web-monorepo-boilerplate',
   description: 'Next.js + chakra-ui + TypeScript template',
-  canonical: 'https://nextarter-chakra.sznm.dev',
+  canonical: 'https://web-monorepo-boilerplate.sznm.dev',
   openGraph: {
-    url: 'https://nextarter-chakra.sznm.dev',
-    title: 'nextarter-chakra',
+    url: 'https://web-monorepo-boilerplate.sznm.dev',
+    title: 'web-monorepo-boilerplate',
     description: 'Next.js + chakra-ui + TypeScript template',
     images: [
       {
-        url: 'https://og-image.sznm.dev/**nextarter-chakra**.sznm.dev.png?theme=dark&md=1&fontSize=125px&images=https%3A%2F%2Fsznm.dev%2Favataaars.svg&widths=250',
-        alt: 'nextarter-chakra.sznm.dev og-image',
+        url: 'https://og-image.sznm.dev/**web-monorepo-boilerplate**.sznm.dev.png?theme=dark&md=1&fontSize=125px&images=https%3A%2F%2Fsznm.dev%2Favataaars.svg&widths=250',
+        alt: 'web-monorepo-boilerplate.sznm.dev og-image',
       },
     ],
-    site_name: 'nextarter-chakra',
+    site_name: 'web-monorepo-boilerplate',
   },
   twitter: {
-    handle: '@sozonome',
+    handle: '@sebpalluel',
     cardType: 'summary_large_image',
   },
 };
--- apps/web/src/pages/_document.tsx
+++ apps/web/src/pages/_document.tsx
@@ -5,7 +5,7 @@ import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 import customTheme from '../lib/styles/customTheme';
 
-const APP_NAME = 'nextarter-chakra';
+const APP_NAME = 'web-monorepo-boilerplate';
 
 class MyDocument extends Document {
   static getInitialProps(ctx: DocumentContext) {
